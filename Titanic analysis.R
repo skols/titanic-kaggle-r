8df7fc8a15d9ab85bace06c6cb8a41f4ec816fb3
Michael Skolnik <mikeskolnik75@gmail.com>
2017-09-01 12:23:26 -0400

    Third commit - Survival Prediction

--- Titanic analysis.R
+++ Titanic analysis.R
@@ -240,3 +240,51 @@ full_data$Mother <- factor(full_data$Mother)
 
 # Double check to make sure there is no more missing data
 md.pattern(titanic)
+
+# PREDICTION
+# Split back into train and test sets
+train <- full_data[1:891, ]
+test <- full_data[892:1309, ]
+
+# Build a model using randomForest
+# Set a random seed
+set.seed(754)
+
+# Buid the model(note: not all possible variables are used)
+rf_model <- randomForest(factor(Survived) ~ Pclass + Sex + Age + SibSp + Parch + Fare +
+                           Embarked + Title + FsizeD + Child + Mother,
+                         data=train)
+
+# Show model error
+plot(rf_model, ylim=c(0,0.36))
+legend("topright", colnames(rf_model$err.rate), col=1:3, fill=1:3)
+# This shows that we're more successful at predicting death than survival
+
+# Look at variable importance
+# Get importance
+importance <- importance(rf_model)
+varImportance <- data.frame(Variables=row.names(importance),
+                            Importance=round(importance[, "MeanDecreaseGini"], 2))
+
+# Create a rank variable based on importance
+rankImportance <- varImportance %>% 
+  mutate(Rank = paste0("#", dense_rank(desc(Importance))))
+
+# Use ggplot2 to visualize relative importance of variables
+ggplot(rankImportance, aes(x=reorder(Variables, Importance), y=Importance, fill=Importance)) +
+  geom_bar(stat="identity") +
+  geom_text(aes(x=Variables, y=0.5, label=Rank),
+            hjust=0, vjust=0.55, size=4, color="red") +
+  labs(x="Variables") +
+  coord_flip() +
+  theme_few()
+
+# Prediction!
+# Predict using the test set
+prediction <- predict(rf_model, test)
+
+# Save the solution to a dataframe with two columns: PassengerId and Survived(prediction)
+solution <- data.frame(PassengerID = test$PassengerId, Survived = prediction)
+
+# Write the solution to a file
+write.csv(solution, file="rf_mod_Solution.csv", row.names=T)
